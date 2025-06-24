9baed814da97077fe886508dabb4cde961e55bed
Ryan Bettin <138413056+ryanbettin@users.noreply.github.com>
2025-06-24 19:34:55 -0300

    Import de navegação de abas

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Navegação de abas
+
 import Navbar from './src/components/Navbar';
 
 // Auth Screens
